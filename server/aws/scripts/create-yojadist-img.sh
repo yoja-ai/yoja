bf2c6f7c86b9497fb96c04da04c4c8416c0c2637
Jagane Sundar <jagane@sundar.org>
2024-06-16 21:01:41 -0700

    added convenience script to push custom container to yojadist public ecr repo

---
+++ server/aws/scripts/create-yojadist-img.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+aws --profile yojadist ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/r9b3c9q7
+(cd lambdas; docker build -t yoja-img .)
+docker tag yoja-img:latest public.ecr.aws/r9b3c9q7/yoja-img:latest
+docker push public.ecr.aws/r9b3c9q7/yoja-img:latest
