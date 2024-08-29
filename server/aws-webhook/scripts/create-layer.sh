cdd69fb8684b8f789c6620d01e84dd431d83c139
Jagane Sundar <jagane@sundar.org>
2024-08-29 09:25:29 -0700

    improvements to make ecs work

--- server/aws-webhook/scripts/create-layer.sh
+++ server/aws-webhook/scripts/create-layer.sh
@@ -7,13 +7,13 @@
 # echo Using AWS Credentials Profile "$1"
 # echo Using AWS Region "$2"
 
-sudo /bin/rm -f /tmp/yoja-webhook-layer.zip
+#/bin/rm -f /tmp/yoja-webhook-layer.zip
 sudo /bin/rm -rf ./layer-build
 mkdir -p layer-build/python/lib/python3.11/site-packages
 (cd layer-build; echo "cryptography" > requirements.txt; echo "google-api-python-client" >> requirements.txt; echo "google-auth-httplib2" >> requirements.txt; echo "google-auth-oauthlib" >> requirements.txt)
-(cd layer-build; sudo docker run -v "$PWD":/var/task "public.ecr.aws/sam/build-python3.11:latest" /bin/sh -c "pip install -r requirements.txt -t python/lib/python3.11/site-packages/; exit")
+(cd layer-build; docker run -v "$PWD":/var/task "public.ecr.aws/sam/build-python3.11:latest" /bin/sh -c "pip install -r requirements.txt -t python/lib/python3.11/site-packages/; exit")
 sudo /bin/rm -rf layer-build/python/lib/python3.11/site-packages/googleapiclient/discovery_cache/documents
-(cd layer-build; sudo zip -r /tmp/yoja-webhook-layer.zip .)
+#(cd layer-build; zip -r /tmp/yoja-webhook-layer.zip .)
 # aws --profile "$1" --region "$2" lambda publish-layer-version --layer-name yoja-webhook-layer \
 #     --description "Yoja Webhook Layer" \
 #     --license-info "MIT" \
